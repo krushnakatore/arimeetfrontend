0e3c05822daed5511ea86e9bd34e177e995c9b39
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2022-03-24 12:35:48 +0530

    created react app

---
+++ src/components/Single/Single.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./single.css";
+
+export const Single = () => {
+  return <div>Single</div>;
+};
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbarWrapper">
+      <div>logo</div>
+      <div>Home</div>
+      <div>Wishlist</div>
+    </div>
+  );
+};
