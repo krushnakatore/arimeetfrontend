d90946ce1474e58f5bfe6a2d61123315f43fe9f9
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2022-03-24 12:49:14 +0530

    added loading while fetching the data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,39 +2,64 @@ import React, { useEffect, useState } from "react";
 import "./home.css";
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const [checked, setChecked] = useState([]);
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
+    setIsLoading(true);
     await fetch("http://localhost:3456/product")
       .then((res) => res.json())
       .then((res) => {
+        // console.log(res);
+        setIsLoading(false);
         setChecked(res);
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   };
 
+  const handleDelete = (ele) => {
+    let arr = checked.filter((e) => {
+      return e._id !== ele._id;
+    });
+    setChecked(arr);
+  };
+
+  const handleAdd = () => {};
+
   return (
     <div>
-      <div className="homeAllList">
-        {checked.map((ele, ind) => {
-          return (
-            <div className="homeList" key={ele._id}>
-              <input type="checkbox" value={ele} />
-              <div>{ele.title}</div>
-              <div>
-                <img src={ele.img} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="homeAllList">
+          {checked.map((ele, ind) => {
+            return (
+              <div className="homeList" key={ele._id}>
+                <input type="checkbox" value={ele} />
+                <div>{ele.title}</div>
+                <div>
+                  <img src={ele.img} />
+                </div>
+                <button
+                  onClick={() => {
+                    handleDelete(ele);
+                  }}
+                >
+                  Delete
+                </button>
+                <button onClick={handleAdd}>Add to Wishlist</button>
               </div>
-              <button onClick={() => {}}>Delete</button>
-              <button>Add to Wishlist</button>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
